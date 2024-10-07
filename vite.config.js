e156bba81c8e9f5cced26d66e7a23398039aef4e
Natriera <49688079+Natriera@users.noreply.github.com>
2024-10-07 21:18:34 +0200

    Ouverture auto et sur chrome

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    open: true, // Ouvre automatiquement le navigateur à l'adresse http://localhost:5173/
+  },
 })
