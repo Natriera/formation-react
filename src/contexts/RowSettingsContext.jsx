46b50dd03aa285007aa27af3785d8cf930a16c57
Natriera <nat.riera@live.fr>
2024-10-09 22:55:29 +0200

    init du bodyColor

--- src/contexts/RowSettingsContext.jsx
+++ src/contexts/RowSettingsContext.jsx
@@ -5,7 +5,7 @@ export const RowSettingsContext = createContext();
 
 export const RowSettingsProvider = ({ children }) => {
   const [rowHeaderColor, setHeaderRowColor] = useState('#ffdffb');
-  const [rowBodyColor, setBodyRowColor] = useState('rgb(230, 212, 189)');
+  const [rowBodyColor, setBodyRowColor] = useState('#f4f4f4');
   const [rowPaddingTop, setRowPaddingTop] = useState(15); // Hauteur par défaut en pixels
   const [rowMarginBottom, setRowMarginBottom] = useState(10); // Hauteur par défaut en pixels
   const [borderRadius, setBorderRadius] = useState(12); // Border radius par défaut
